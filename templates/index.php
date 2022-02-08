8f6ae4f7ab60e557d1701fb491d8401c3e9cf795
Mouronval Clément <clement.mouronval@gmail.com>
2022-02-08 11:50:24 +0100

    update css

--- templates/index.php
+++ templates/index.php
@@ -1,6 +1,6 @@
 <?php
 script('intranetagglo', ['app.63670f42','chunk-vendors.756499e2']);
-style('intranetagglo', ['app.c449553e','chunk-vendors.61d5ee76']);
+style('intranetagglo', ['app.d1af05f6','chunk-vendors.61d5ee76']);
 ?>
 
 <div id="app">
