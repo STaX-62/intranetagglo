9d49e34ff14cd120d4a62adedd7de80bf4e7c772
Mouronval Clément <clement.mouronval@gmail.com>
2022-03-11 15:16:25 +0100

    update

--- templates/index.php
+++ templates/index.php
@@ -1,6 +1,6 @@
 <?php
-script('intranetagglo2', 'intranetagglo');
-style('intranetagglo2', 'intranetagglo');
+script('intranetagglo', 'intranetagglo2');
+style('intranetagglo', 'intranetagglo2');
 ?>
 
 <div id="app">
