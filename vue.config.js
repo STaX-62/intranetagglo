46e36bc627856e00565aaf9beae2769ae253404f
Mouronval Clément <clement.mouronval@gmail.com>
2022-03-29 09:41:34 +0200

    update

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = {
     pages: {
-        intranetagglo86: {
+        intranetagglo88: {
             // entry for the page
             entry: 'src/main.js',
         },
