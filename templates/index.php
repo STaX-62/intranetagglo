8c303198995437c362bffb9347a552048b1c4392
Mouronval Clément <clement.mouronval@gmail.com>
2022-05-09 15:00:17 +0200

    update

--- templates/index.php
+++ templates/index.php
@@ -1,6 +1,6 @@
 <?php
-script('intranetagglo', 'intranetagglo120');
-style('intranetagglo', 'intranetagglo120');
+script('intranetagglo', 'intranetagglo121');
+style('intranetagglo', 'intranetagglo121');
 ?>
 
 <div id="app">
--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = {
     pages: {
-        intranetagglo120: {
+        intranetagglo121: {
             // entry for the page
             entry: 'src/main.js',
         },
