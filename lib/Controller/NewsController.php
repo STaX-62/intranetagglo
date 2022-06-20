4e9296ec9068d1cff4cfe626e79ddc02eab3db45
Mouronval Clément <clement.mouronval@gmail.com>
2022-06-20 11:42:33 +0200

    Update NewsController.php

--- lib/Controller/NewsController.php
+++ lib/Controller/NewsController.php
@@ -20,7 +20,7 @@ use OCP\IURLGenerator;
 use OCP\Notification\INotification;
 use OCP\IntranetAgglo\Service\NotificationService;
 
-class newscontroller extends Controller
+class NewsController extends Controller
 {
     /** @var NewsService */
     private $service;
