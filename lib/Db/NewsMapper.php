2145cc0c3da4688914f3c213f65c28d0e667e771
Mouronval Clément <clement.mouronval@gmail.com>
2022-06-22 11:47:56 +0200

    Update NewsMapper.php

--- lib/Db/NewsMapper.php
+++ lib/Db/NewsMapper.php
@@ -144,7 +144,7 @@ class NewsMapper extends QBMapper
 
 
 
-        return [$this->findEntities($qb2), $row['count'], $time];
+        return [$this->findEntities($qb2), $row['count'], $time, $qb2->getSql()];
     }
 
     /**
