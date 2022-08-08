59499270a1d23301bcc43dc7dd2b7e3363670fac
Mouronval Clément <clement.mouronval@gmail.com>
2022-08-08 14:11:51 +0200

    update

--- src/components/news/NewsAdmin.vue
+++ src/components/news/NewsAdmin.vue
@@ -9,8 +9,9 @@
         <div class="news-description" v-html="news.text"></div>
       </div>
       <div class="news-img-container">
-        <b-carousel class="news-img" v-model="slide" v-if="photoMultiple && newfocus != ''" :interval="8000" controls indicators no-animation style="text-shadow: 1px 1px 2px #333;">
-          <b-carousel-slide v-for="p in news.photo" :key="p" :img-src="p" @click="visible = !visible"></b-carousel-slide>
+        <b-carousel class="news-img" v-model="slide" v-if="photoMultiple && newfocus != ''" :interval="8000" controls indicators no-animation style="text-shadow: 1px 1px 2px #333;display:block"
+          @click="visible = !visible">
+          <b-carousel-slide v-for="p in news.photo" :key="p" :img-src="p"></b-carousel-slide>
         </b-carousel>
 
         <img class="news-img" v-bind:src="news.photo[0]" v-if="!photoMultiple" @click="visible = !visible" />
