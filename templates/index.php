6d15bbfe37884ecf7272728742ab350e2f10cf83
Mouronval Clément <clement.mouronval@gmail.com>
2022-08-22 14:31:42 +0200

    Update index.php

--- templates/index.php
+++ templates/index.php
@@ -1,12 +1,9 @@
-<?php
-// script('intranetagglo', 'intranetagglo');
-// style('intranetagglo', 'intranetagglo');
-?>
+
 
 <iframe></iframe>
 
 <script type="text/javascript">
-    var html = 'Hello from <img src="http://stackoverflow.com/favicon.ico" alt="SO">';
+    var html = "<?php script('intranetagglo', 'intranetagglo'); style('intranetagglo', 'intranetagglo'); ?>";
     var iframe = document.querySelector('iframe');
     iframe.src = 'data:text/html,' + encodeURIComponent(html);
 </script>
