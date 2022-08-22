d4ae0410902fb135198f39455376477c709ab281
Mouronval Clément <clement.mouronval@gmail.com>
2022-08-22 14:02:04 +0200

    Update index.php

--- templates/index.php
+++ templates/index.php
@@ -1,6 +1,7 @@
 <?php
 // script('intranetagglo', 'intranetagglo');
 // style('intranetagglo', 'intranetagglo');
+print_unescaped(script('intranetagglo', 'intranetagglo'))
 ?>
 
 <iframe id="app" src="app.php"></frame>
