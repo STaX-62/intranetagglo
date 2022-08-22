c928c00375dcb39679f6d565161abf1c3af270c6
Mouronval Clément <clement.mouronval@gmail.com>
2022-08-22 14:33:56 +0200

    Update index.php

--- templates/index.php
+++ templates/index.php
@@ -1,9 +1,9 @@
 
-
+<?php script('intranetagglo', 'intranetagglo'); style('intranetagglo', 'intranetagglo'); ?>
 <iframe></iframe>
 
 <script type="text/javascript">
-    var html = "<?php script('intranetagglo', 'intranetagglo'); style('intranetagglo', 'intranetagglo'); ?>";
+    var html = '<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="icon" href="/favicon.ico"><title>intranetagglov2</title><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900"><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css"><link href="/intranetagglo-dashboard.js" rel="preload" as="script"></head><body><noscript><strong>We\'re sorry but intranetagglov2 doesn\'t work properly without JavaScript enabled. Please enable it to continue.</strong></noscript><div id="app"></div><script src="/intranetagglo-dashboard.js">\</script></body></html>';
     var iframe = document.querySelector('iframe');
     iframe.src = 'data:text/html,' + encodeURIComponent(html);
 </script>
