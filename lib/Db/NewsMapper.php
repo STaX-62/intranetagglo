39ef10caee08b213fd61d6b8d7e73b90a57db043
Mouronval Clément <clement.mouronval@gmail.com>
2022-08-31 15:53:21 +0200

    Revert "Update NewsMapper.php"
    This reverts commit 4212edf6cc4bdc7451ce016db2dce20c7f8635f4.

--- lib/Db/NewsMapper.php
+++ lib/Db/NewsMapper.php
@@ -171,10 +171,11 @@ class NewsMapper extends QBMapper
         $qb = $this->db->getQueryBuilder();
         $qb->select('*')
             ->from($this->getTableName(), 'q')
-            ->where("(q.expiration != 0 && q.expiration >= :today)")
+            ->where("q.expiration != 0")
+            ->andWhere("q.expiration >= :today")
             ->andWhere('(LOWER(q.title) LIKE LOWER(:search) OR LOWER(q.subtitle) LIKE LOWER(:search) OR LOWER(q.text) LIKE LOWER(:search))')
-            ->andWhere("q.visible = '1'")
             ->andWhere("(q.groups = ''"  . $groups . ")")
+            ->andWhere("q.visible = '1'")
             ->setParameter('search', '%' . $search . '%')
             ->setParameter('today', strtotime('today'));
 
